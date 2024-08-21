700e94cb5a99a07463e9ba41c35f8d87430fcb52
Abhinav Baral <abhinavbaral@gmail.com>
2024-08-21 16:49:49 +0545

    Js TriaL

---
+++ Assets/JS/script.js
@@ -0,0 +1,7 @@
+let arr = [
+    {songName : "Over the Rainbow",url:"./Assets/song/Somewhere Over the Rainbow - The Wizard of Oz (1_8) Movie CLIP (1939) HD.mp3",img:".Assets/Img/OIP.jpg"},
+    {songName : "As Time Goes By",url:"./Assets/song/Casablanca - As Time Goes By - Original Song by Sam (Dooley Wilson).mp3",img:".Assets/Img/R.jpg"},
+    {songName : "Singinin the Rain",url:"./Assets/song/Singin in the Rain (Full Song_Dance - 52) - Gene Kelly - Musical Romantic Comedies - 1950s Movies.mp3",img:".Assets/Img/R (1).jpg"},
+    {songName : "The Sound of Music",url:"./Assets/song/The Sound of Music  - THE SOUND OF MUSIC (1965).mp3",img:".Assets/Img/OIP (1).jpg"}
+]
+console.log(arr)
